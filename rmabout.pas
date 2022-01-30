8007f7668d0395cddb429b2b7accd4865217f7c6
Nick's Hardware Youtube Channel <56885781+nickshardware@users.noreply.github.com>
2022-01-29 20:40:59 -0500

    Update rmabout.pas

--- rmabout.pas
+++ rmabout.pas
@@ -8,7 +8,7 @@ uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,lclintf;
 
 Const
-  ProgramName ='Raster Master v1.0 Beta R31';
+  ProgramName ='Raster Master v1.0 Beta R32';
   ProgramLicense = 'Released under MIT License';
 
 type
